b69750d13093aa286be2f7c3085b788066cfccf8
Aurelien JEOFFRAY <aurelien.jeoffray@ajdeveloppement.org>
2023-12-22 18:05:25 +0100

    Passage de la version de compatibilité debian de 7 à 9

--- packager/makedeb.sh
+++ packager/makedeb.sh
@@ -18,7 +18,7 @@ rm concoursjeunes_$VERSION.orig.tar.gz
 #copie le debian
 mkdir debian
 #cp ../../../../packager/linux/debian/compat debian/
-echo 7 > debian/compat
+echo 9 > debian/compat
 cp ../../../../packager/linux/debian/control debian/
 cp ../../../../packager/linux/debian/copyright debian/
 cp ../../../../packager/linux/debian/dirs debian/
